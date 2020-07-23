24d9cc860b97cad5af4399e9bfb09c5c9c1cad1d
Jeremiah Trnka <jeremiah@jeremiahs-mbp.lan>
2020-07-23 15:35:22 -0700

    Updated form validation schema and and cypress test script.

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -4,14 +4,18 @@ const formSchema = yup.object().shape({
   name: yup
     .string()
     .min(1,'Name must be at least one character')
+    .max(50, 'Too many characters')
     .required('Name is required'),
   email: yup
     .string()
     .email('Must be a valid email')
+    .max(50, 'Too many characters')
     .required('Email is required'),
   password: yup
     .string()
     .min(6, 'Password must be at least six characters')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*_\-\(\)\?])(?=.{8,})/, 'Your password isn\'t strong enough yet')
+    .max(50, 'Are you really going to remember that password?')
     .required('Password is required'),
   tos: yup
     .boolean()
